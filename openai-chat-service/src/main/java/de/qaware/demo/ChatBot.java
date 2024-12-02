10909451aaf0637304a15ab89ccc384676cfb904
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2024-12-02 23:49:38 +0100

    Added memory provider example.

--- openai-chat-service/src/main/java/de/qaware/demo/ChatBot.java
+++ openai-chat-service/src/main/java/de/qaware/demo/ChatBot.java
@@ -1,17 +1,29 @@
 package de.qaware.demo;
 
+import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 
-@RegisterAiService(tools = {ChatBotTools.class})
+@RegisterAiService(
+    tools = ChatBotTools.class,
+    chatMemoryProviderSupplier = ChatBotMemoryProvider.class
+)
 public interface ChatBot {
 
     @SystemMessage("""
-            You are an AI named Bob answering questions about Qaware GmbH.
+            You are an AI named Bob answering questions about QAware GmbH.
             Your response must be polite, use the same language as the question, and be relevant to the question.
 
             When you don't know, respond that you don't know the answer.
             """)
-    String chat(@UserMessage String question);
+    String ask(@UserMessage String question);
+
+    @SystemMessage("""
+            You are an AI named Alice chatting about QAware GmbH.
+            Your responses must be polite, use the same language as the question, and be relevant to the questions.
+
+            When you don't know, respond that you don't know the answer.
+            """)
+    String chat(@MemoryId int memoryId, @UserMessage String message);
 }
--- openai-chat-service/src/main/java/de/qaware/demo/ChatBotResource.java
+++ openai-chat-service/src/main/java/de/qaware/demo/ChatBotResource.java
@@ -1,5 +1,6 @@
 package de.qaware.demo;
 
+import dev.langchain4j.agent.tool.P;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -7,15 +8,23 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/api/ask")
+@Path("/api")
 public class ChatBotResource {
 
     @Inject
     ChatBot bot;
   
     @GET
+    @Path("/ask")
     @Produces(MediaType.TEXT_PLAIN)
     public String ask(@QueryParam("q") String question) {
-        return bot.chat(question);
+        return bot.ask(question);
+    }
+
+    @GET
+    @Path("/chat")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String chat(@QueryParam("id") int id, @QueryParam("msg") String message) {
+        return bot.chat(id, message);
     }
 }
