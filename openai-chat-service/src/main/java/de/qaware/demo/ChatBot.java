6179b5f6f4fa78047e80f944d2f6eafc2e99897e
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2024-12-03 01:57:51 +0100

    Fixed chat endpoint.

--- openai-chat-service/src/main/java/de/qaware/demo/ChatBot.java
+++ openai-chat-service/src/main/java/de/qaware/demo/ChatBot.java
@@ -1,14 +1,17 @@
 package de.qaware.demo;
 
 import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.Moderate;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
+import jakarta.enterprise.context.ApplicationScoped;
 
 @RegisterAiService(
     tools = ChatBotTools.class,
     chatMemoryProviderSupplier = ChatBotMemoryProvider.class
 )
+@ApplicationScoped
 public interface ChatBot {
 
     @SystemMessage("""
@@ -17,13 +20,14 @@ public interface ChatBot {
 
             When you don't know, respond that you don't know the answer.
             """)
+    @Moderate
     String ask(@UserMessage String question);
 
     @SystemMessage("""
             You are an AI named Alice chatting about QAware GmbH.
             Your responses must be polite, use the same language as the question, and be relevant to the questions.
 
-            When you don't know, respond that you don't know the answer.
+            When you don't know, respond that you don't know the answer.            
             """)
     String chat(@MemoryId int memoryId, @UserMessage String message);
 }
